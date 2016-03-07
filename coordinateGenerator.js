0eb8c7a03f975597e21dbd63fbd928c33f055551
tbenyon <tom.benyon@gmail.com>
2016-03-07 20:50:04 +0000

    Make computer random shots only in every other cell

--- coordinateGenerator.js
+++ coordinateGenerator.js
@@ -1,6 +1,11 @@
 exports.getRandomCoords = function() {
-    var x = Math.floor((Math.random() * 10));
-    var y = Math.floor((Math.random() * 10));
+    var x;
+    var y;
+    do {
+        x = Math.floor((Math.random() * 10));
+        y = Math.floor((Math.random() * 10));
+    } while ((x + y) % 2 === 1);
+
     return {
         'x': x,
         'y': y
